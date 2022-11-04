f676d35903e04d43ee6e8b39232882f0c330edf4
Aleksthor <athorset98@gmail.com>
2022-11-04 18:54:42 +0100

    ImGUI

--- Hazel/src/Hazel/Application.h
+++ Hazel/src/Hazel/Application.h
@@ -21,12 +21,18 @@ namespace Hazel
 		void PushLayer(Layer* layer);
 		void PushOverlay(Layer* overlay);
 
+		inline static Application& Get() { return *s_Instance; }
+		inline Window& GetWindow() { return *m_Window; }
+
 	private:
 		std::unique_ptr<Window> m_Window;
 		bool m_Running = true;
 		bool onWindowClosed(WindowCloseEvent& e);
 
 		LayerStack m_LayerStack;
+
+	private:
+		static Application* s_Instance;
 
 	};
 
--- Hazel/src/Hazel/Layer.h
+++ Hazel/src/Hazel/Layer.h
@@ -19,8 +19,8 @@ namespace Hazel
 		virtual void OnEvent(Event& event) {}
 
 		inline const std::string& GetName() const { return m_DebugName; }
-	protected:
-		std::string m_DebugName;
+	private:
+		const std::string m_DebugName;
 
 	};
 }
